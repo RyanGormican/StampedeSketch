12249f3e2f3470001ef7699332de17fc426c4cdf
RyanGormican <ryan.gormican@gmail.com>
2023-08-31 01:11:54 -0600

    a
    a

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -4,7 +4,7 @@
       <a href="https://www.linkedin.com/in/ryangormican/">
         <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
       </a>
-      <a href="https://github.com/RyanGormican/CardCache">
+      <a href="https://github.com/RyanGormican/StampedeSketch">
         <Icon icon="mdi:github" color="#e8eaea" width="60" />
       </a>
       <a href="https://ryangormicanportfoliohub.vercel.app/">
@@ -14,18 +14,21 @@
     <div class="title">
       StampedeSketch
     </div>
+    <Timer :duration="10"></Timer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  import { defineComponent } from 'vue';
+  import { Icon } from '@iconify/vue';
+  import Timer from '@/components/Timer.vue';
+  export default defineComponent({
+  name: 'Main',
+  components: {
+  Icon,
+  Timer,
   },
-});
+  });
 </script>
 
 <style scoped>
---
+++ frontend/src/components/Timer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+   {{remainingTime}}
+  </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import { Icon } from '@iconify/vue';
+  export default defineComponent({
+  name: 'Timer',
+  components: {
+  Icon,
+  },
+  date() {
+  return {
+  remainingTime: this.duration,
+  timeInterval: null,
+  };
+  },
+  props: {
+  duration: {
+    type:Number,
+    required: true,
+  },
+  methods : {
+  startTimer(){
+  this.timeInterval = setInterval(() => {
+  if (this.remainingTime > 0) {
+    this.remainingTime--;
+  }
+  },1000);
+  }
+  },
+  mounted() {
+  this.startTimer();
+  },
+  }});
+</script>
+
+<style scoped>
+  @import '@/assets/styles.css';
+
+</style>
