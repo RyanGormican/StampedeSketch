095919570cfdeec812058cfce2a94e0c54a5c8d5
RyanGormican <ryan.gormican@gmail.com>
2023-08-30 23:02:16 -0600

    a
    a

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
+});
